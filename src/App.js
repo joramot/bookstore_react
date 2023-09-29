e33e7e8e0be8d2da9b3b3ddc73242ee231cda72d
JOSE RAUL MOJICA <mojica.raul@gmail.com>
2023-09-28 23:08:24 -0600

    Setup app, install react-router-dom

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Bookstore</h1>
+    </div>
+  );
+}
+
+export default App;
