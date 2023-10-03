97b8e1c604068ff24e6509a19e8f53121d86e3e5
JOSE RAUL MOJICA <mojica.raul@gmail.com>
2023-10-03 16:08:00 -0600

    Add Provider and pass store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </React.StrictMode>
+    </Provider>
   </BrowserRouter>,
 );
